f44d62057008a55fa741745e7fe9de16f05117d4
hamza1851 <hamza.dev011@gmail.com>
2024-10-23 18:19:48 +0530

    Add AppContext

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,44 @@
+import React, { createContext, useState, useEffect, useContext } from "react"
+
+// Create the context
+const AppContext = createContext()
+
+// Custom hook for consuming context
+export const useAppContext = () => {
+  return useContext(AppContext)
+}
+
+// Provider component to wrap around the app
+export const AppProvider = ({ children }) => {
+  const [searchTerm, setSearchTerm] = useState("") //Track term entered by the user
+  const [countryData, setCountryData] = useState([]) //Contains countries data
+  const [selectedRegion, setSelectedRegion] = useState("") // Track the selected region
+  const [regions, setRegions] = useState([]) // Track available regions
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const resp = await fetch("https://restcountries.com/v3.1/all")
+      const data = await resp.json()
+      setCountryData(data)
+
+      const uniqueRegions = [...new Set(data.map((country) => country.region))]
+      setRegions(uniqueRegions)
+    }
+    fetchCountries()
+  }, [])
+
+  return (
+    <AppContext.Provider
+      value={{
+        searchTerm,
+        setSearchTerm,
+        countryData,
+        selectedRegion,
+        setSelectedRegion,
+        regions,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
