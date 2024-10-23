d3e1a2caa3b41916ad9d188667b77a2b853c1bf9
hamza1851 <hamza.dev011@gmail.com>
2024-10-23 18:24:16 +0530

    Fixed Navbar padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,14 @@ import { MdOutlineDarkMode } from "react-icons/md"
 const Navbar = () => {
   return (
     <>
-      <nav className="shadow-xl flex items-center p-5 mb-8">
+      <nav className="shadow-xl flex items-center p-5 mb-8 px-auto">
         <h1 className="ml-20 mr-auto font-bold text-2xl">
           Where in the world ?
         </h1>
-        <darkBar className="flex align-middle">
+        <div className="flex align-middle">
           <MdOutlineDarkMode className="mr-2 h-5" />
           <p className="mr-2">Dark Mode</p>
-        </darkBar>
+        </div>
       </nav>
     </>
   )
