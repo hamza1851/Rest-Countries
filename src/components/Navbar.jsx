3a83abdd55543af2b1fbce4fcf00313372b399d9
hamza1851 <hamza.dev011@gmail.com>
2024-10-23 09:40:48 +0530

    Modified diemensions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ import { MdOutlineDarkMode } from "react-icons/md"
 const Navbar = () => {
   return (
     <>
-      <nav className="shadow-xl flex align-middle p-5 ">
+      <nav className="shadow-xl flex items-center p-5 mb-8">
         <h1 className="ml-20 mr-auto font-bold text-2xl">
           Where in the world ?
         </h1>
         <darkBar className="flex align-middle">
-          <MdOutlineDarkMode className="mr-2 h-6" />
+          <MdOutlineDarkMode className="mr-2 h-5" />
           <p className="mr-2">Dark Mode</p>
         </darkBar>
       </nav>
