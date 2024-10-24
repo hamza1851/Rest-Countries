ef0efda780945ac0a625e562a520dded25d27d96
hamza1851 <hamza.dev011@gmail.com>
2024-10-24 17:33:50 +0530

    Add SortBy compoenent

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -2,11 +2,13 @@ import React from "react"
 import SearchField from "./SearchField"
 import FilterBar from "./FilterBar"
 import Subregion from "./SubRegion"
+import SortBy from "./SortBy"
 
 const SearchSection = () => {
   return (
     <div className="max-w-[80%] mx-auto p-2 px-0 md:flex justify-between ">
       <SearchField />
+      <SortBy />
       <Subregion />
       <FilterBar />
     </div>
