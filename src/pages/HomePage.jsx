d364efed00d1c2c835ed9d50226952af39e96089
hamza1851 <hamza.dev011@gmail.com>
2024-10-25 18:26:32 +0530

    Seperated Navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,10 @@
 import React from "react"
-import Navbar from "../components/Navbar"
 import SearchSection from "../components/SearchSection"
 import Cards from "../components/Cards"
 import { AppProvider } from "../context/AppContext"
 const HomePage = () => {
   return (
     <AppProvider>
-      <Navbar />
-
       <SearchSection />
       <Cards />
     </AppProvider>
