82845efe3c9e7ecea8f1d8b0cc7e7d6b0f9c9867
hamza1851 <hamza.dev011@gmail.com>
2024-10-26 11:10:35 +0530

    Seperated provider to main

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,12 @@
 import React from "react"
 import SearchSection from "../components/SearchSection"
 import Cards from "../components/Cards"
-import { AppProvider } from "../context/AppContext"
 const HomePage = () => {
   return (
-    <AppProvider>
+    <>
       <SearchSection />
       <Cards />
-    </AppProvider>
+    </>
   )
 }
 
